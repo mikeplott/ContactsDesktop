602c70adfaa6de440b8e7d782cc6463ed1c88781
michaelplott <mike.plott80@gmail.com>
2016-09-27 13:36:38 -0400

    Added interface and creating FXML fields.

---
+++ src/sample/Contact.java
@@ -0,0 +1,33 @@
+package sample;
+
+/**
+ * Created by michaelplott on 9/27/16.
+ */
+public class Contact {
+    String name;
+    String phoneNumber;
+    String email;
+
+    public Contact(String name, String phoneNumber, String email) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + phoneNumber + " " + email;
+    }
+}
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,36 @@
 package sample;
 
-public class Controller {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+
+    ObservableList<Contact> contacts = FXCollections.observableArrayList();
+
+    @FXML
+    String contactName;
+
+    @FXML
+    String contactNumber;
+
+    @FXML
+    String contactEmail;
+
+    public void addContact() {
+
+    }
+
+    public void removeContact() {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
